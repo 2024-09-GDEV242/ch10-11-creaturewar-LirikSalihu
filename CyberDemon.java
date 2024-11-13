e390d45391ffe620dda3d1ae90f17167f02b83cb
Lirik Salihu <G00304223@stu.raritanval.edu>
2024-11-12 21:37:09 -0500

    Create CyberDemon.java

---
+++ CyberDemon.java
@@ -0,0 +1,19 @@
+
+/**
+ * Represents a cyber demon creature with strength and health.
+ * Inherits special attack behavior from the Demon class.
+ *
+ * @author Lirik Salihu
+ * @version 2024.11.12
+ */
+public class CyberDemon extends Demon {
+
+    /**
+     * Constructs a CyberDemon with the specified strength and health.
+     * @param strength the strength of the cyber demon
+     * @param health the health of the cyber demon
+     */
+    public CyberDemon(int strength, int health) {
+        super(strength, health);
+    }
+}
